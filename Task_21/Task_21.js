4aaa27d2b65d108051d6361dad31978ae051ac68
Asjad Nawaz Khan <asjadnawaz2002@gmail.com>
2024-06-25 19:00:20 +0500

    Task_21 Commited

---
+++ Task_21/Task_21.js
@@ -0,0 +1,8 @@
+/* They think of something you could store in a TypeScript Object.
+Write a program that creates Objects containing these items. */
+var person = {
+    name: "Asjad",
+    age: 22,
+    gender: "Male"
+};
+console.log(person);
---
+++ Task_21/Task_21.ts
@@ -0,0 +1,11 @@
+/* They think of something you could store in a TypeScript Object. 
+Write a program that creates Objects containing these items. */
+
+let person: any = {
+    name: "Asjad",
+    age: 22,
+    gender: "Male"
+}
+
+console.log(person);
+
